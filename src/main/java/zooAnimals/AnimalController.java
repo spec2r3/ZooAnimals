0fea9f2a68c8a6f1e34372ae9a2b8eb9149dd73e
spec2r3 <allanjonesbiju@gmail.com>
2021-10-14 11:09:21 +0100

    aws

--- src/main/java/zooAnimals/AnimalController.java
+++ src/main/java/zooAnimals/AnimalController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping(path = "api/v1/animal")
+@RequestMapping
 
 public class AnimalController{
 
